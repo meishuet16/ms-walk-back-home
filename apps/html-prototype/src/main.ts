fafa6342dc70d01a3da532bd576dfa3728540437
meishuet <meishuetlee8@gmail.com>
2026-08-31 17:33:27 +0800

    feat(capsule): install capsule machine bridge

--- apps/html-prototype/src/main.ts
+++ apps/html-prototype/src/main.ts
@@ -3,12 +3,14 @@ import { installAuthoredCutsceneLifecycleBridge } from "./systems/AuthoredCutsce
 import { installLabisLifecycleBridge } from "./systems/LabisLifecycleBridge.js";
 import { installRecordsScrollStabilityBridge } from "./systems/RecordsScrollStabilityBridge.js";
 import { installReflectionWallUi } from "./systems/ReflectionWallUiBridge.js";
+import { installCapsuleMachineBridge } from "./systems/CapsuleMachineBridge.js";
 import { SceneDebugEditor } from "./systems/SceneDebugEditor.js";
 
 installAuthoredCutsceneLifecycleBridge(WalkBackHomeApp.prototype);
 installLabisLifecycleBridge(WalkBackHomeApp.prototype);
 installRecordsScrollStabilityBridge(WalkBackHomeApp.prototype);
 installReflectionWallUi(WalkBackHomeApp.prototype);
+installCapsuleMachineBridge(WalkBackHomeApp.prototype);
 
 const root = document.querySelector<HTMLElement>("#app")!;
 if (new URLSearchParams(window.location.search).get("debug") === "scene") {
