2e34248bb91892b0ae0fb1183d27d8f38ac74c44
meishuet <meishuetlee8@gmail.com>
2026-08-31 13:42:18 +0800

    fix(reflection): install continuous wall behavior

--- apps/html-prototype/src/main.ts
+++ apps/html-prototype/src/main.ts
@@ -6,6 +6,7 @@ import { installReflectionWallOverlayCleanupBridge } from "./systems/ReflectionW
 import { installReflectionWallPaperBoundsBridge } from "./systems/ReflectionWallPaperBoundsBridge.js";
 import { installReflectionWallPaperCatalog } from "./systems/ReflectionWallPaperCatalogBridge.js";
 import { installReflectionWallVisualPolishBridge } from "./systems/ReflectionWallVisualPolishBridge.js";
+import { installReflectionWallContinuumBridge } from "./systems/ReflectionWallContinuumBridge.js";
 import { SceneDebugEditor } from "./systems/SceneDebugEditor.js";
 
 installAuthoredCutsceneLifecycleBridge(WalkBackHomeApp.prototype);
@@ -14,6 +15,7 @@ installReflectionWallPaperCatalog();
 installReflectionWallExperienceBridge(WalkBackHomeApp.prototype);
 installReflectionWallPaperBoundsBridge(WalkBackHomeApp.prototype);
 installReflectionWallVisualPolishBridge(WalkBackHomeApp.prototype);
+installReflectionWallContinuumBridge(WalkBackHomeApp.prototype);
 installReflectionWallOverlayCleanupBridge(WalkBackHomeApp.prototype);
 
 const root = document.querySelector<HTMLElement>("#app")!;
