c34656bf1fa4896317f2156a33d1af1e102ebd57
meishuet <meishuetlee8@gmail.com>
2026-09-02 01:28:29 +0800

    Keep ffmpeg build path intact

--- apps/html-prototype/scripts/build.mjs
+++ apps/html-prototype/scripts/build.mjs
@@ -59,7 +59,7 @@ for (const stylesheet of localStylesheets) await access(resolve(root, "dist", st
 
 await mkdir(resolve(root, "dist/browser/ffmpeg"), { recursive: true });
 await copyFile(resolve(root, "../../node_modules/@ffmpeg/core/dist/esm/ffmpeg-core.js"), resolve(root, "dist/browser/ffmpeg/ffmpeg-core.js"));
-await copyFile(resolve(root, "../../node_modules/@ffmpeg/core/dist/esm/ffmpeg-core.wasm"), resolve(root, "dist/browser/ffmpeg-core.wasm"));
+await copyFile(resolve(root, "../../node_modules/@ffmpeg/core/dist/esm/ffmpeg-core.wasm"), resolve(root, "dist/browser/ffmpeg/ffmpeg-core.wasm"));
 for (const file of ["worker.js", "const.js", "errors.js"]) {
   await copyFile(resolve(root, `../../node_modules/@ffmpeg/ffmpeg/dist/esm/${file}`), resolve(root, `dist/browser/${file}`));
 }
