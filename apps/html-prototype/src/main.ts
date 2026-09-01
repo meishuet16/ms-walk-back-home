5044a93ba7aa75f598d9d005b35e27e4edf04eab
meishuet <meishuetlee8@gmail.com>
2026-09-01 23:33:03 +0800

    Install story route experience bridge

--- apps/html-prototype/src/main.ts
+++ apps/html-prototype/src/main.ts
@@ -11,6 +11,7 @@ import { installCapsuleKeptOrganizerBridge } from "./systems/CapsuleKeptOrganize
 import { installLocalBackupSupplementBridge } from "./systems/LocalBackupSupplementBridge.js";
 import { installChapterMusicBridge } from "./systems/ChapterMusicBridge.js";
 import { installFinalDreamBridge } from "./systems/FinalDreamBridge.js";
+import { installStoryRouteBridge } from "./systems/StoryRouteBridge.js";
 import { SceneDebugEditor } from "./systems/SceneDebugEditor.js";
 
 installAuthoredCutsceneLifecycleBridge(WalkBackHomeApp.prototype);
@@ -25,6 +26,7 @@ installCapsuleKeptOrganizerBridge();
 installLocalBackupSupplementBridge(WalkBackHomeApp.prototype);
 installChapterMusicBridge(WalkBackHomeApp.prototype as unknown as { enterCurrentMemory?: () => Promise<void>; finishReturnToForest?: () => void });
 installFinalDreamBridge(WalkBackHomeApp.prototype as unknown as { enterCurrentMemory?: () => Promise<void> });
+installStoryRouteBridge(WalkBackHomeApp.prototype as unknown as Parameters<typeof installStoryRouteBridge>[0]);
 
 const root = document.querySelector<HTMLElement>("#app")!;
 if (new URLSearchParams(window.location.search).get("debug") === "scene") {
