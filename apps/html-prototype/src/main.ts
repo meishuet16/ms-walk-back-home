198eedf8f673897b44a6125c8949ff70f47c69ee
meishuet <meishuetlee8@gmail.com>
2026-09-02 14:57:24 +0800

    Wire Labis Story Route completion safeguard

--- apps/html-prototype/src/main.ts
+++ apps/html-prototype/src/main.ts
@@ -16,6 +16,7 @@ import { installFinalDreamBridge } from "./systems/FinalDreamBridge.js";
 import { initializeStoryRouteStartup, installStoryRouteBridge } from "./systems/StoryRouteBridge.js";
 import { installStoryRouteMobileFixBridge } from "./systems/StoryRouteMobileFixBridge.js";
 import { initializeStoryRouteFollowups, installStoryRouteFollowupBridge } from "./systems/StoryRouteFollowupBridge.js";
+import { initializeStoryRouteLabisCompletion, installStoryRouteLabisCompletionBridge } from "./systems/StoryRouteLabisCompletionBridge.js";
 import { SceneDebugEditor } from "./systems/SceneDebugEditor.js";
 
 installAuthoredCutsceneLifecycleBridge(WalkBackHomeApp.prototype);
@@ -33,6 +34,7 @@ installFinalDreamBridge(WalkBackHomeApp.prototype as unknown as { enterCurrentMe
 installStoryRouteBridge(WalkBackHomeApp.prototype as unknown as Parameters<typeof installStoryRouteBridge>[0]);
 installStoryRouteMobileFixBridge(WalkBackHomeApp.prototype as unknown as Parameters<typeof installStoryRouteMobileFixBridge>[0]);
 installStoryRouteFollowupBridge(WalkBackHomeApp.prototype as unknown as Parameters<typeof installStoryRouteFollowupBridge>[0]);
+installStoryRouteLabisCompletionBridge(WalkBackHomeApp.prototype as unknown as Parameters<typeof installStoryRouteLabisCompletionBridge>[0]);
 
 const root = document.querySelector<HTMLElement>("#app")!;
 if (new URLSearchParams(window.location.search).get("debug") === "scene") {
@@ -41,6 +43,7 @@ if (new URLSearchParams(window.location.search).get("debug") === "scene") {
   const app = new WalkBackHomeApp(root);
   initializeStoryRouteStartup(app);
   initializeStoryRouteFollowups(app);
+  initializeStoryRouteLabisCompletion(app);
   initializeReflectionNoteRemove(app);
   initializeCapsuleFullscreenBridge(app);
 }
