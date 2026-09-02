37154ecdac19e9a3d9bf395a7f21f619fe75750e
meishuet <meishuetlee8@gmail.com>
2026-09-02 15:22:45 +0800

    Wire Final Dream epilogue polish

--- apps/html-prototype/src/main.ts
+++ apps/html-prototype/src/main.ts
@@ -12,6 +12,7 @@ import { installCapsuleAudioCaptureBridge } from "./systems/CapsuleAudioCaptureB
 import { installCapsuleKeptOrganizerBridge } from "./systems/CapsuleKeptOrganizerBridge.js";
 import { installLocalBackupSupplementBridge } from "./systems/LocalBackupSupplementBridge.js";
 import { installChapterMusicBridge } from "./systems/ChapterMusicBridge.js";
+import { installFinalDreamEpiloguePolishBridge } from "./systems/FinalDreamEpiloguePolishBridge.js";
 import { installFinalDreamBridge } from "./systems/FinalDreamBridge.js";
 import { initializeStoryRouteStartup, installStoryRouteBridge } from "./systems/StoryRouteBridge.js";
 import { installStoryRouteMobileFixBridge } from "./systems/StoryRouteMobileFixBridge.js";
@@ -30,6 +31,7 @@ installCapsuleAudioCaptureBridge();
 installCapsuleKeptOrganizerBridge();
 installLocalBackupSupplementBridge(WalkBackHomeApp.prototype);
 installChapterMusicBridge(WalkBackHomeApp.prototype as unknown as { enterCurrentMemory?: () => Promise<void>; finishReturnToForest?: () => void });
+installFinalDreamEpiloguePolishBridge();
 installFinalDreamBridge(WalkBackHomeApp.prototype as unknown as { enterCurrentMemory?: () => Promise<void> });
 installStoryRouteBridge(WalkBackHomeApp.prototype as unknown as Parameters<typeof installStoryRouteBridge>[0]);
 installStoryRouteMobileFixBridge(WalkBackHomeApp.prototype as unknown as Parameters<typeof installStoryRouteMobileFixBridge>[0]);
