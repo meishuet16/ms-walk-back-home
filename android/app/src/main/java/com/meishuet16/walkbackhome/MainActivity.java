9cd3756f84ebd383730417693bb03c853bfa7a25
meishuet <meishuetlee8@gmail.com>
2026-09-04 01:35:58 +0800

    fix: register external browser plugin

--- android/app/src/main/java/com/meishuet16/walkbackhome/MainActivity.java
+++ android/app/src/main/java/com/meishuet16/walkbackhome/MainActivity.java
@@ -1,5 +1,13 @@
 package com.meishuet16.walkbackhome;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ExternalBrowserPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
